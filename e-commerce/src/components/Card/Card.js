78ea4cd002ae35ee8ad038ffc7085a2f3d1cbdb0
michaelrushton-dev <michaelwjrushton@gmail.com>
2022-09-02 13:39:04 +0100

    displaying items correctly

--- e-commerce/src/components/Card/Card.js
+++ e-commerce/src/components/Card/Card.js
@@ -1,11 +1,13 @@
-function Card(item) {
-    return <div>Card
-    <h1>{item.title}</h1>
-    <p>{item.category}</p>
-    <img src={item.image} alt={item.title}></img>
-    <p>{item.description}</p>
-    </div>;
-
+function Card({ item }) {
+    return (
+        <div>
+            Card
+            <h1>{item.title}</h1>
+            <p>{item.category}</p>
+            <img src={item.image} alt={item.title}></img>
+            <p>{item.description}</p>
+        </div>
+    );
 }
 
 export default Card;
