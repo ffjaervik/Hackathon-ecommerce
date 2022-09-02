bb49ff4061043f71ba374c883c8c51057b4c8460
Philip Kaminski <philip.doge.kaminski@gmail.com>
2022-09-02 14:35:06 +0100

    justified/align items inside the card

--- e-commerce/src/components/Card/Card.js
+++ e-commerce/src/components/Card/Card.js
@@ -1,19 +1,18 @@
 function Card({ item }) {
-    return (
-        <div className='flex flex-col bg-slate-500 w-full'>
-            Card
-            <h1>{item.title}</h1>
-            <p>{item.category}</p>
-            <div className=' image-container max-w-52 min-h-52'>
-                <img
+  return (
+    <div className="flex flex-col bg-slate-500  items-center justify-between">
+      <h1>{item.title}</h1>
+      <p>{item.category}</p>
+      <div className=" image-container bg-green-600 w-52 h-52">
+        {/* <img
                     className='image  object-scale-down '
                     src={item.image}
                     alt={item.title}
-                ></img>
-            </div>
-            <p>{item.description}</p>
-        </div>
-    );
+                ></img> */}
+      </div>
+      <p>{item.description}</p>
+    </div>
+  );
 }
 
 export default Card;
