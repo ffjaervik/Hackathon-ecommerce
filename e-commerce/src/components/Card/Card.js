37de20b912f8b78210eb01c4942f0464dd71cff2
Philip Kaminski <philip.doge.kaminski@gmail.com>
2022-09-02 14:56:40 +0100

    changed gaps inside the card

--- e-commerce/src/components/Card/Card.js
+++ e-commerce/src/components/Card/Card.js
@@ -1,14 +1,16 @@
 function Card({ item }) {
   return (
-    <div className="flex flex-col bg-slate-500  items-center justify-between">
-      <h1 className = "text-lg font-bold" >{item.title}</h1>
-      <p>{item.category}</p>
-      <div className=" flex image-container bg-white w-52 h-52 items-center justify-center">
-        <img
-                    className='image object-cover max-h-52 '
-                    src={item.image}
-                    alt={item.title}
-                ></img>
+    <div className="flex flex-col bg-slate-500  items-center justify-around">
+      <div className="flex flex-col items-center">
+        <h1 className="text-lg font-bold">{item.title}</h1>
+        <p>{item.category}</p>
+        <div className=" flex image-container bg-white w-52 h-52 items-center justify-center">
+          <img
+            className="image object-cover max-h-52 "
+            src={item.image}
+            alt={item.title}
+          ></img>
+        </div>
       </div>
       <p>{item.description}</p>
     </div>
