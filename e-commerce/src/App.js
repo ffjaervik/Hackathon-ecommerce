3f1ebb012e8136778d54d1a68a61c6931521b777
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-09-02 10:58:03 +0200

    tailwind & react setup

---
+++ e-commerce/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<div className='w-full sm:w-6 md:w-9 lg:w-20 pl-9 ml-12'>
+<p className='bg-color[white]'>Hello World</p>
+ 
+</div>
+  );
+}
+
+export default App;
