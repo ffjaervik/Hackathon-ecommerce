e0dfaceeeeabbeae7fe4281e1df8b837a2ac2e77
Kunal S <kunals@Kunals-iMac.local>
2022-09-02 14:53:16 +0100

    stylised the titles and centered the images

--- e-commerce/src/components/Card/Card.js
+++ e-commerce/src/components/Card/Card.js
@@ -1,14 +1,14 @@
 function Card({ item }) {
   return (
     <div className="flex flex-col bg-slate-500  items-center justify-between">
-      <h1>{item.title}</h1>
+      <h1 className = "text-lg font-bold" >{item.title}</h1>
       <p>{item.category}</p>
-      <div className=" image-container bg-green-600 w-52 h-52">
-        {/* <img
-                    className='image  object-scale-down '
+      <div className=" flex image-container bg-white w-52 h-52 items-center justify-center">
+        <img
+                    className='image object-cover max-h-52 '
                     src={item.image}
                     alt={item.title}
-                ></img> */}
+                ></img>
       </div>
       <p>{item.description}</p>
     </div>
